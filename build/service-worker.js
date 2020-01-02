4c6060ec7ce3f5d89507556d477ff32cb51a20ca
Atiksh Srivastava <ayushs125@gmail.com>
2020-01-02 20:32:29 +0530

    added footer

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2294bd08d1629082001406aa19bda2f.js"
+  "/precache-manifest.a72bfe96301dd0ea222a79182ef04081.js"
 );
 
 self.addEventListener('message', (event) => {
